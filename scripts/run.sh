fd4809bfec1d2575f312da745a691f628336d04c
Johan Klokkhammer Helsing <johan.helsing@qt.io>
2018-05-16 16:02:43 +0200

    Build qtwayland examples

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 set -ex
+
 #time scripts/clone-qt.sh
 #echo Finished cloning
+
 time scripts/checkout-qt.sh
 echo Finished preparing sources
+
 time scripts/build-qt.sh
 echo Finished building
+
 time scripts/build-qtbase-tests.sh
 echo Finished building qtbase tests
+
 time scripts/build-qtwayland-tests.sh
 echo Finished building qtwayland tests
+
+time scripts/build-qtwayland-examples.sh
+echo Finished building qtwayland examples
+
 time scripts/test-qt.sh
 echo Finished testing
